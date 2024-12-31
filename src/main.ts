a407db083f362eea4605c1d22e35b45f36f82318
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2024-12-31 19:35:11 +0600

    delete functionality added

--- src/main.ts
+++ src/main.ts
@@ -19,5 +19,48 @@ myForm.onsubmit = (e: SubmitEvent) => {
     id: String(Math.random() * 100),
   };
   todos.push(todo);
+  todoInput.value = "";
+  renderTodo(todos);
   console.log(todos, "4 noline");
+};
+
+const generateTodoItem = (title: string, isCompleted: boolean, id: string) => {
+  const todo = document.createElement("div") as HTMLDivElement;
+  todo.className = "todo";
+
+  // creating a checkbox...
+  const checkBox = document.createElement("input") as HTMLInputElement;
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.className = "isCompleted";
+  checkBox.checked = isCompleted;
+  checkBox.onchange = () => {
+    paragraph.className = checkBox.checked ? "textCut" : "";
+  };
+
+  // crating p for title..
+  const paragraph = document.createElement("p") as HTMLParagraphElement;
+  paragraph.innerText = title;
+  // creating delete button..
+  const btn = document.createElement("button") as HTMLButtonElement;
+  btn.innerText = "X";
+  btn.className = "deleteBtn";
+  btn.onclick = () => {
+    deletTodo(id);
+  };
+
+  // appending all to todo..
+  todo.append(checkBox, paragraph, btn);
+  todoConatainer.append(todo);
+};
+
+const deletTodo = (id: string) => {
+  const idx = todos.findIndex((index) => index.id === id);
+  todos.splice(idx, 1);
+  renderTodo(todos);
+};
+const renderTodo = (todos: Todo[]) => {
+  todoConatainer.innerText = "";
+  todos.forEach((item) => {
+    generateTodoItem(item.title, item.isCompleted, item.id);
+  });
 };
